3d0c823234cfbac91ed3ac10bc6cd6e7cc5cc6b5
ypfect <363825455@qq.com>
2019-09-19 18:25:49 +0800

    1.订单相关分离

--- src/main/java/com/overstar/es/constants/EnumErrorCode.java
+++ src/main/java/com/overstar/es/constants/EnumErrorCode.java
@@ -12,12 +12,6 @@ import lombok.Getter;
 @Getter
 public enum EnumErrorCode {
     INDEX_ERROR("索引异常", 10001),
-    ORDER_VALIDATE_ERROR("订单参数验证失败", 20001),
-    ORDER_DATA_CALCULATE_ERROR("订单算价失败", 20002),
-    ORDER_DATA_PACKAGE_PREPARE_ERROR("订单参数封装失败", 20003),
-    ORDER_PERSIS_ERROR("订单持久化失败", 20004),
-    ORDER_DELAY_QUEUE_ERROR("订单加入延时队列失败", 20005),
-    ORDER_SERVICE_INIT_ERROR("订单处理handler初始化失败", 20006),
     ;
     private String desc;
     private Integer code;
